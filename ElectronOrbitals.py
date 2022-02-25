e3e3fe7ebfca9a8a278b023851bdd3bfbcbb0b42
simonchemnitz <55578391+simonchemnitz@users.noreply.github.com>
2022-02-25 16:46:47 +0100

    Update ElectronOrbitals.py
    Added functions

--- ElectronOrbitals.py
+++ ElectronOrbitals.py
@@ -1,3 +1,4 @@
+#Packages
 import numpy as np
 import pandas as pd
 from scipy.special import sph_harm, genlaguerre
@@ -6,3 +7,45 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 
+
+#Functions
+def hwave(n, m, l, r, theta, phi):
+    """
+    Calculate The normalized position wavefunctions
+    in spherical coordinates
+    Parameters
+    ----------
+    n : int
+        Principle quantum number
+    m : int
+        Magnetic quantum number
+    l : int
+        Azimuthal quantum number
+    r : float
+        Radius
+    theta : float
+        angle theta in radians
+    phi : float
+        angle phi in radians
+    
+    returns
+    ----------
+    psi(r,theta,phi) : float
+    """
+    #a0star = (4*pi*epsilon0*hbar**2)/(mu*np.exp(2))
+    a0star = 1
+    rho = (2*r)/(n*a0star)
+    root = np.sqrt( ( ( (2)/(a0star) )**3 )*( (np.math.factorial(n-l-1))/(2*n*(np.math.factorial(n+l) ) ) ) )
+    e = np.exp(-rho/2)
+    L = genlaguerre(n-l-1,2*l+1)(rho)
+    Y = sph_harm(m, l, theta, phi)
+    return np.real(root*e*L*Y)
+
+def appendSpherical_np(xyz):
+    ptsnew = np.hstack((xyz, np.zeros(xyz.shape)))
+    xy = xyz[:,0]**2 + xyz[:,1]**2
+    ptsnew[:,3] = np.sqrt(xy + xyz[:,2]**2)
+    ptsnew[:,4] = np.arctan2(np.sqrt(xy), xyz[:,2]) # for elevation angle defined from Z-axis down
+    #ptsnew[:,4] = np.arctan2(xyz[:,2], np.sqrt(xy)) # for elevation angle defined from XY-plane up
+    ptsnew[:,5] = np.arctan2(xyz[:,1], xyz[:,0])
+    return ptsnew
