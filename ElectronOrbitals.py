3533d6e93a7f14539d2662c61e8b0219420b778a
simonchemnitz <55578391+simonchemnitz@users.noreply.github.com>
2022-02-25 16:46:19 +0100

    Create ElectronOrbitals.py

---
+++ ElectronOrbitals.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+from scipy.special import sph_harm, genlaguerre
+
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+
